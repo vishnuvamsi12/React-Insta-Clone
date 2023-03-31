b07c36a8a605a9ad3dba3aa5d0f901b45f04c129
vishnuvamsi12 <vishnuvamsi8074@gmail.com>
2023-03-31 16:56:27 +0530

    Added List Card and seperate Header

---
+++ my-app/src/components/Post/Header/Header.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from "../../../images/circle_icon.svg";
+import Camera from "../../../images/camera_icon.png";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+export default function Header(){
+    return (
+        
+    <>
+        <Navbar>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={Logo} alt="Logo" />
+                    <span>Insta Clone</span>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Link to="/post/new">
+                        <img src={Camera} alt="Logo" />
+                    </Link>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    </>
+
+    )
+
+}
---
+++ my-app/src/components/Post/New/NewPost.js
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from "../../../images/circle_icon.svg";
+import Camera from "../../../images/camera_icon.png";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import "./NewPost.css";
+import Header from "../Header/Header";
+
+export default function NewPost(){
+
+    const [formData, setFormData] = useState({
+        image:null,
+        author:"",
+        location:"",
+        description:""
+    })
+
+    function CaptureForm(event){
+        event.preventDefault();
+        const formValues = new FormData(event.target);
+    }
+
+    return (
+        
+    <>
+        <Header />
+            
+        <div className="new-post-container">
+            <Card>
+                <Card.Body>
+                    <Form onSubmit={CaptureForm}>
+
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Control name="Image" type="file" />
+                        </Form.Group>
+
+                        <InputGroup className="mb-3">
+                            <Form.Control aria-label="Author" name="author" placeholder="Enter Name" />
+                            <Form.Control aria-label="Location" name="location" placeholder="Enter Location"  />
+                        </InputGroup>
+
+                        <Form.Control size="lg" type="text" name="description" placeholder="Description" />
+
+                        <div className="btn-container">
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </div>
+
+                    </Form>
+                </Card.Body>
+            </Card>
+        </div>
+    </>
+    )
+}
